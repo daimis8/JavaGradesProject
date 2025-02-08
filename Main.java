cc6c4b454a90f5c11c959f9b3b8d38d0024de7d4
Daimonas Jocys <daimis@Daimonass-MacBook-Pro.local>
2025-02-08 10:54:31 +0200

    A simple user input of data with exception of letter(no validation for grades)

--- Main.java
+++ Main.java
@@ -1,5 +1,30 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        String name;
+        int mathGrade;
+        int englishGrade;
+        int biologyGrade;
+        double avg;
+        Students student1;
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.println("What is the students name?");
+            name = scanner.nextLine();
+
+            System.out.println("What is students math grade?");
+            mathGrade = scanner.nextInt();
+
+            System.out.println("What is students english grade?");
+            englishGrade = scanner.nextInt();
+
+            System.out.println("What is students biology grade?");
+            biologyGrade = scanner.nextInt();
+        }
+        catch(InputMismatchException e) {
+            System.out.println("Enter a number!");
+        }
         
     }
 }
