e2d697228e662380d1ea04cc8fdd0096c1f50fde
Daimonas Jocys <daimis@Daimonass-MacBook-Pro.local>
2025-02-08 10:33:18 +0200

    Seting up students class

--- Students.java
+++ Students.java
@@ -4,4 +4,30 @@ public class Students {
     int englishGrade;
     int biologyGrade;
     double avg;
+
+    Students(String name, int mathGrade, int englishGrade, int biologyGrade, double avg) {
+        this.name = getName();
+        this.mathGrade = getMathGrade();
+        this.englishGrade = getEnglishGrade();
+        this.biologyGrade = getBiologyGrade();
+        this.avg = getAvg();
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getMathGrade() {
+        return mathGrade;
+    }
+    public int getEnglishGrade() {
+        return englishGrade;
+    }
+    public int getBiologyGrade() {
+        return biologyGrade;
+    }
+    public double getAvg() {
+        return avg;
+    }
+
+
 }
